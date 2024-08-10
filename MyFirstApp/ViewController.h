1a3440c3e97f37c96d4fbed22d371fa10aa07ad3
Kumar Alok <kumaralok.s@rently.com>
2024-08-11 02:29:28 +0530

    Complete Cat Project

--- MyFirstApp/ViewController.h
+++ MyFirstApp/ViewController.h
@@ -6,8 +6,20 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AudioToolbox/AudioToolbox.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    
+    SystemSoundID soundID;
+}
+
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+
+- (IBAction)action:(id)sender;
+
+
 
 
 @end
--- MyFirstApp/ViewController.m
+++ MyFirstApp/ViewController.m
@@ -16,7 +16,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.label.hidden = YES;
+    
+    NSURL *soundURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"Cat" ofType:@"wav"]];
+    
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef)soundURL, & soundID);
+    
 }
 
+
+
+
+- (IBAction)action:(id)sender {
+    
+    self.label.hidden = NO;
+    self.label.text = @"Meow !!!!!";
+    
+    AudioServicesPlaySystemSound(soundID);
+    
+    [NSTimer scheduledTimerWithTimeInterval:2 target: self selector: @selector (hideLabel) userInfo:nil repeats:NO];
+}
+
+-(void) hideLabel {
+    self.label.hidden = YES;
+}
 
 @end
